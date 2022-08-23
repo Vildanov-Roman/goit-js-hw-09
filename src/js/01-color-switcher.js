5ea5a1cd85ecfa1494fcaa509f5db20ca4002b99
Vildanov-Roman <vildanovv.rr@gmail.com>
2022-08-23 14:03:56 +0300

    app hw9 part1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,12 +4,9 @@ const startBtn = document.querySelector('button[data-start]')
 const stopBtn = document.querySelector('button[data-stop]')
 const body = document.querySelector('body')
 
-stopBtn.disabled = true;
 let intervalID = null;
 
 const randomColor = {
-  DELAY: 500,
-  
   getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   },
@@ -17,23 +14,20 @@ const randomColor = {
   interval() {
     intervalID = setInterval(() => {
       changeBgc();
-    }, this.DELAY);
-    stopBtn.disabled = false;
+    }, 1000);
   },
 
   start() {
     startBtn.addEventListener('click', () => {
       this.interval();
-      startBtn.disabled = true;
       stopBtn.disabled = false;
+      startBtn.disabled = true;
     });
-    stopBtn.addEventListener('click', this.stop);
-  },
-  
-  stop() {
-    clearInterval(intervalID);
-    stopBtn.disabled = true;
-    startBtn.disabled = false;
+    stopBtn.addEventListener('click', () => {
+      clearInterval(intervalID);
+      startBtn.disabled = false;
+      stopBtn.disabled = true;
+    });
   },
 }
 
@@ -42,69 +36,3 @@ function changeBgc() {
 }
 
 randomColor.start();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import '../css/common.css';
-
-// const bodyEl = document.querySelector('body');
-// const startEl = document.querySelector('button[data-start]');
-// const stopEl = document.querySelector('button[data-stop]');
-// stopEl.disabled = true;
-// let intervalID = null;
-
-// const randomBodyColorGenerator = {
-//   DELAY: 1000,
-
-//   getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-//   },
-
-//   interval() {
-//     intervalID = setInterval(() => {
-//       changeBgColorRandom();
-//     }, this.DELAY);
-//     stopEl.disabled = false;
-//   },
-
-//   start() {
-//     startEl.addEventListener('click', () => {
-//       this.interval();
-//       startEl.disabled = true;
-//       stopEl.disabled = false;
-//     });
-//     stopEl.addEventListener('click', this.stop);
-//   },
-
-//   stop() {
-//     clearInterval(intervalID);
-//     stopEl.disabled = true;
-//     startEl.disabled = false;
-//   },
-// };
-
-// function changeBgColorRandom() {
-//   bodyEl.style.backgroundColor = `${randomBodyColorGenerator.getRandomHexColor()}`;
-// }
-
-// randomBodyColorGenerator.start();
-
